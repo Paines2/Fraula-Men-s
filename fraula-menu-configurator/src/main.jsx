28e7aad596f1aa445e4578f86be4e683dce99dba
Paines2 <49285243+Paines2@users.noreply.github.com>
2025-04-16 15:10:04 +0200

    Delete fraula-menu-configurator directory

--- fraula-menu-configurator/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MenuConfigurator from './menu'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MenuConfigurator />
-  </React.StrictMode>,
-)
